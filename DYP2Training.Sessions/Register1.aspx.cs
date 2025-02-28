54c72eaa1f57112ee03315212ffcc3cd0c9ecf51
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-02-28 17:46:28 +0530

    dotnet project day 1 part 1

---
+++ DYP2Training.Sessions/Register1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DYP2Training.Sessions
+{
+    public partial class Register1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into Users values (@name,@contact,@city)", con);
+            cmd.Parameters.AddWithValue("@name",txtName.Text);
+            cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+            cmd.Parameters.AddWithValue("@city", ddlCity.SelectedItem.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            txtContact.Text = "";
+            txtName.Text = "";
+            ddlCity.SelectedIndex = 0;
+
+            Response.Write("<script>alert('Data Saved..!')</script>");
+        }
+    }
+}
