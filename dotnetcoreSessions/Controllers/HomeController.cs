3ed9256688af5a64ae250b9dd40bd878290fd753
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-03-04 18:00:23 +0530

    dotnet core session 1

---
+++ dotnetcoreSessions/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using dotnetcoreSessions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcoreSessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //int a = 70;
+            //string name = "Test";
+            //return View("Index",name);
+
+            Student student = new Student();
+            student.ID = 3;
+            student.Name = "Ram";
+            student.City = "Mumbai";
+            return View(student);
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
