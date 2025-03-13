9f206433a6211436e7d34492e75b46e5aa7b06ad
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-03-13 16:29:02 +0530

    dotnet core session 3

--- dotnetcoreSessions/Controllers/AboutController.cs
+++ dotnetcoreSessions/Controllers/AboutController.cs
@@ -11,6 +11,7 @@ namespace dotnetcoreSessions.Controllers
 
         public IActionResult History()
         {
+
             return View();
         }
     }
--- dotnetcoreSessions/Program.cs
+++ dotnetcoreSessions/Program.cs
@@ -1,11 +1,22 @@
 
 //DI Container
+using dotnetcoreSessions.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//passing connectionstring to ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options=>options.UseSqlServer(builder.Configuration.
+    GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
