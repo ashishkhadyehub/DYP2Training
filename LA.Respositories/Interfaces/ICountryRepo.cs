e4327cc7902a841d9670637f09ebda6f80884bea
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-03-25 18:00:07 +0530

    dotnet core session 6

---
+++ LA.Respositories/Interfaces/ICountryRepo.cs
@@ -0,0 +1,23 @@
+﻿using LA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA.Respositories.Interfaces
+{
+    public interface ICountryRepo
+    {
+        IEnumerable<Country> GetAll();
+
+        Country GetById(int id);
+
+        void Save(Country country);
+
+        void Edit(Country country);
+
+        void RemoveData(Country country);
+
+    }
+}
---
+++ LA.Respositories/Interfaces/IStateRepo.cs
@@ -0,0 +1,24 @@
+﻿using LA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA.Respositories.Interfaces
+{
+    public interface IStateRepo
+    {
+        IEnumerable<State> GetAll();
+
+        State GetById(int id);
+
+        void Save(State state);
+
+        void Edit(State state);
+
+        void RemoveData(State state);
+
+
+    }
+}
